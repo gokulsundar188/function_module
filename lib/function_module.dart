b6fb3752f8eb3a0ad4e84c3f878c51e225aaaef4
Gokul Sundar <gokul.sundar@golden-hippo.com>
2020-12-23 18:23:51 +0530

    Basic method added

--- lib/function_module.dart
+++ lib/function_module.dart
@@ -1,7 +1,14 @@
 library function_module;
 
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class Functions {
+  // /// Returns [value] plus 1.
+  // int addOne(int value) => value + 1;
+  // void main() {
+  //   helloWorld();
+  // }
+
+  String helloWorld() {
+    return "hello World";
+  }
 }
