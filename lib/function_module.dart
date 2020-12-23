9829a75819be1b9b7155becae940451e2ec011e1
Gokul Sundar <gokul.sundar@golden-hippo.com>
2020-12-23 19:11:30 +0530

    changes

--- lib/function_module.dart
+++ lib/function_module.dart
@@ -1,14 +1,6 @@
 library function_module;
 
-import 'package:flutter/cupertino.dart';
-
-/// A Calculator.
 class Functions {
-  // /// Returns [value] plus 1.
-  // int addOne(int value) => value + 1;
-  // void main() {
-  //   helloWorld();
-  // }
 
   String helloWorld() {
     return "Hello World";
