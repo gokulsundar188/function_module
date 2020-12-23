bc028343c05625ab300796ef4fe8be2b64f14212
Gokul Sundar <gokul.sundar@golden-hippo.com>
2020-12-23 19:26:29 +0530

    toast message added

--- lib/function_module.dart
+++ lib/function_module.dart
@@ -1,12 +1,18 @@
 library function_module;
 
-class Functions {
+import 'package:flutter/cupertino.dart';
+import 'package:toast/toast.dart';
 
+class Functions {
   String helloWorld() {
     return "Hello World";
   }
 
-  addition(int num1, int num2) {
-    return (num1 + num2).toString();
+  addition(int num1, int num2, BuildContext context) {
+    if ((num1 == null && num2 == null) || (num1 == "" || num2 == "")) {
+      Toast.show("please enter valid number", context);
+    } else {
+      return (num1 + num2).toString();
+    }
   }
 }
